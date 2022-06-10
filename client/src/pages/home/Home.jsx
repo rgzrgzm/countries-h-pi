d72e0df09fe21879da2b23fdd04d7a88c2cea00e
rgzrgzm <mariomkt97@gmail.com>
2022-06-10 15:06:07 -0700

    12th commit - fixing bug inputSearch & filter by act

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -107,12 +107,14 @@ const Home = () => {
       />
 
       {/* If user search a country on InputSearch => Searched view */}
-      {isSearched && (
+      {isSearched && !isFilteredByActivity && (
         <div className="searched">
           <button onClick={() => setIsSearched(false)}>Back</button>
           {searchedCountries?.map((country) => (
             <Card key={country.id} country={country} />
           ))}
+
+          {searchedCountries.length === 0 && "City not found"}
         </div>
       )}
 
